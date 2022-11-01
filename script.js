a2733ce3e2a8f9996d5be64ebf7d21f135a6ec55
tarekahmed196 <tarekjustcse@gmail.com>
2022-11-01 19:41:57 +0600

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var a = prompt("Enter UserName");
 var b = prompt("Enter password");
-if (a == "Koly" && b == "05.11.1998") {
+if (a == "Tamu" && b == "02.11.1998") {
 	window.location.href = "start.html";
 }
 else {
